2a2e9ed2590d8cc6efafb6d5378162d9f981ed8e
nahjd <nahidmemmedov711@gmail.com>
2024-08-23 18:58:12 +0400

    MERN

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ function Hero() {
                 <h3 className="text-white text-2xl lg:text-4xl mb-2 flex items-center justify-center gap-2">
                     Hii I'M
                     <img
-                        style={{ width: "50px", height: "50px", marginTop: "15px" }}
+                        style={{ width: "70px", height: "70px" }}
                         src="https://media.tenor.com/QpTLQALtdskAAAAi/hii-wave.gif"
                         alt="hii"
                         className="h-10"
@@ -17,7 +17,7 @@ function Hero() {
                 <span className="text-white text-3xl lg:text-5xl font-bold mb-4">
                     <span className="text-[#ad50eb]">Nahid Memmedov</span>
                 </span>
-                <span className="text-[#ad50eb] text-2xl lg:text-3xl font-semibold h-20 lg:h-32 w-72 lg:w-80 flex items-center justify-center px-5   capitalize">
+                <span className="text-[#ad50eb] text-2xl lg:text-3xl font-semibold h-30 lg:h-32 w-72 lg:w-80 flex items-center justify-center px-5   capitalize">
                     <Type />
                 </span>
             </div>
